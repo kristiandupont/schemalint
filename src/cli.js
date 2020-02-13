fb4b81f20182c7d0739f6c99afb53e93cf18a125
Kristian Dupont <kristian@kristiandupont.com>
2020-02-13 21:41:16 +0100

    Extract

---
+++ src/cli.js
@@ -0,0 +1,61 @@
+import path from 'path';
+import chalk from 'chalk';
+// @ts-ignore
+import optionator from 'optionator';
+import { processDatabase } from './engine';
+// @ts-ignore
+// const { version } = require('../package.json');
+const version = '0.0.1';
+
+async function main() {
+  const o = optionator({
+    prepend: 'Usage: schemalint [options]',
+    append: `Version ${version}`,
+    options: [
+      {
+        option: 'help',
+        alias: 'h',
+        type: 'Boolean',
+        description: 'displays help',
+      },
+      {
+        option: 'config',
+        alias: 'c',
+        type: 'path::String',
+        description:
+          'Use this configuration, overriding .schemalintrc.* config options if present',
+      },
+    ],
+  });
+
+  let options;
+
+  try {
+    options = o.parseArgv(process.argv);
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+
+  if (options.help) {
+    console.log(o.generateHelp());
+    process.exit(0);
+  }
+
+  console.log(`${chalk.greenBright('schema-lint')}`);
+  const configFile = path.join(
+    process.cwd(),
+    options.config || '.schemalintrc.js'
+  );
+  const config = require(configFile);
+
+  try {
+    const exitCode = await processDatabase(config);
+    process.exit(exitCode);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+main();
---
+++ src/engine.js
@@ -0,0 +1,68 @@
+import { keys, indexBy, prop, values } from 'ramda';
+import knex from 'knex';
+import chalk from 'chalk';
+import { extractSchema } from 'extract-pg-schema';
+
+import * as builtinRules from './rules';
+
+const registeredRules = indexBy(prop('name'), values(builtinRules));
+
+let anyIssues = false;
+const suggestedMigrations = [];
+
+function report({ rule, identifier, message, suggestedMigration = null }) {
+  console.log(`${chalk.yellow(identifier)}: error ${chalk.red(rule)} : ${message}`);
+  if (suggestedMigration) {
+    suggestedMigrations.push(suggestedMigration);
+  }
+  anyIssues = true;
+}
+
+export async function processDatabase({ connection, rules, schemas }) {
+  console.log(`Connecting to ${chalk.greenBright(connection.database)} on ${connection.host}`);
+  const knexConfig = {
+    client: 'pg',
+    connection,
+  };
+  const db = knex(knexConfig);
+
+  for (const schema of schemas) {
+    const extractedSchemaObject = await extractSchema(
+      schema.name,
+      schema.tablesToIgnore,
+      db
+    );
+
+    const schemaObject = {
+      name: schema.name,
+      ...extractedSchemaObject,
+    };
+
+    const mergedRules = {
+      ...rules,
+      ...(schema.rules || {}),
+    };
+
+    for (const ruleKey of keys(mergedRules)) {
+      if (!ruleKey in registeredRules) {
+        throw new Error(`Unknown rule: "${ruleKey}"`);
+      }
+      const [state, ...options] = mergedRules[ruleKey];
+      if (state === 'error') {
+        registeredRules[ruleKey].process({ schemaObject, report, options });
+      }
+    }
+  }
+
+  if (anyIssues) {
+    if (suggestedMigrations.length) {
+      console.log('');
+      console.log('Suggested fix');
+      suggestedMigrations.forEach(sf => console.log(sf));
+    }
+    return 1;
+  }
+
+  console.log('No issues detected');
+  return 0;
+}
