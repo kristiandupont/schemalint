efd486cc88290bf5e99d1f04c1445ff52eb24b1f
Kristian Dupont <kristian@kristiandupont.com>
2020-07-08 18:30:17 +0200

    Remove build folder from git

--- build/engine.d.ts
+++
@@ -1,7 +0,0 @@
-export function processDatabase({ connection, plugins, rules, schemas, ignores, }: {
-    connection: any;
-    plugins?: any[];
-    rules: any;
-    schemas: any;
-    ignores?: any[];
-}): Promise<1 | 0>;
